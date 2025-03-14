ee2d63fb670b1a8a84ba6bacac9c60e4696aa59e
Ferchis-Santiago <138594069+Ferchis-Santiago@users.noreply.github.com>
2025-03-14 10:49:18 +0100

    Add files via upload

---
+++ colourRefinement/colorref.py
@@ -0,0 +1,90 @@
+from graph_io import load_graph
+from collections import Counter
+import os
+import glob
+import time
+
+def colour_refinement(graphs):
+    colours = {}
+
+    # Return a sorted list with neighbour colours of the vertex
+    def get_neighbour_colours(vertex, colouring):
+        neighbour_colours = []
+        for neighbour in vertex.neighbours:
+            neighbour_colours.append(colouring[neighbour])
+        return sorted(neighbour_colours)
+
+    # Creates new colouring based on previous one
+    def refine_colours(graph, colouring):
+        nonlocal colours
+        new_colouring = {}
+        last_colour = max(colours.values(), default=max(colouring.values()))
+        partitions = 0
+        # Assign new colours based on given colouring and neighbours colours
+        for vertex in graph:
+            neighbours_colours = tuple(get_neighbour_colours(vertex, colouring))
+            if (colouring[vertex], neighbours_colours) not in colours:
+                partitions += 1
+                last_colour += 1
+                colours[(colouring[vertex], neighbours_colours)] = last_colour
+            new_colouring[vertex] = colours[(colouring[vertex], neighbours_colours)]
+        partitions = len(set(new_colouring.values()))
+        return new_colouring, partitions
+
+    results = []
+    # Process each graph
+    for index, graph in enumerate(graphs):
+        # Start assigning colour 1 to all vertices
+        colouring = {v: v.degree for v in graph.vertices}
+        partitions = max(colouring.values())
+        i = 0
+
+        # Iteratively refine the colouring
+        while True:
+            i += 1
+            new_colouring, new_partitions = refine_colours(graph, colouring)
+
+            colouring = new_colouring
+
+            # Stop iterations if the new colouring is the same as the previous one
+            if new_partitions == partitions:
+                break
+
+            partitions = new_partitions
+
+        # Check if graph is discrete
+        discrete = len(set(colouring.values())) == len(graph.vertices)
+        results.append((index, colouring, i, discrete))
+
+    # Output the results
+    isomorphic_graphs = {}
+    for index, colouring, iterations, discrete in results:
+        colour_sum = sum(colouring.values())
+        freq_dict = dict(Counter(colouring.values()))
+        colour_freq_list = sorted(freq_dict.values())
+        if len(colour_freq_list) == 1:
+            iterations = 0
+        key = (colour_sum, discrete, iterations)
+        if key not in isomorphic_graphs:
+            isomorphic_graphs[key] = ([],colour_freq_list)
+        isomorphic_graphs[key][0].append(index)
+    
+    # Return output
+    return [(indexes[0], indexes[1], colour_count[2], colour_count[1]) for colour_count, indexes in
+            isomorphic_graphs.items()]
+
+
+def basic_colorref(file_path):
+    with open(file_path) as f:
+        L = load_graph(f, read_list=True)
+        graphs = L[0]
+        result = colour_refinement(graphs)
+    return result
+
+#if __name__ == '__main__':
+    start_time = time.perf_counter()
+    result = basic_colorref('Benchmark_instances/CrefBenchmark1.grl')
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(result)
+    print(f"Execution time: {execution_time:.4f} seconds")
